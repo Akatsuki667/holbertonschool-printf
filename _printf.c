d11c837ce9844a1f043f75cd77646b546abe2da1
daalagelerua <aurele284@gmail.com>
2024-11-25 15:37:14 +0000

    ca marche pas bolossE

--- _printf.c
+++ _printf.c
@@ -2,5 +2,31 @@
 
 int _printf(const char *format, ...)
 {
-	
+print_type types[] = {
+{"c", print_char},
+{"s", print_string},
+{NULL, NULL}
+};
+
+va_list args;
+int i = 0;
+int j = 0;
+
+va_start(args, format);
+
+while (format != NULL && format[i] != '\0')
+	{
+	while (types[j].type)
+		{
+		if (*types[j].type == format[i])
+		types[j].f(args);
+		}
+	j++;
+	}
+	j = 0;
+	i++;
+
+_putchar('\n');
+va_end(args);
+return (i);
 }
--- print_func.c
+++ print_func.c
@@ -10,27 +10,13 @@ _putchar(c);
 void print_string(va_list args)
 {
 char *s;
-{
 s = va_arg(args, char*);
 if (s == NULL)
-	{
 	s = "(nil)";
-	}
+
 while (*s)
 	{
 	_putchar(*s);
 	s++;
 	}
-}
-
-void print_int(va_list args)
-{
-int num;
-num = va_arg(args, int);
-_putchar(num + '0');
-}
-
-void print_int_b10(va_list args)
-{
-
 }
